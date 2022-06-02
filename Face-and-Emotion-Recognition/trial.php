938035605e8f728673146e6c1aa512abc828480f
Indra Sigicharla <indhhra@gmail.com>
2022-06-03 02:08:31 +0530

    first commit

---
+++ Face-and-Emotion-Recognition/trial.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Running a Python script</title>
+        <?php
+        echo shell_exec("python video-recog.py");
+        ?>
+        </head>
+    <body>
+        <!-- BODY -->
+    </body>
+</html>
